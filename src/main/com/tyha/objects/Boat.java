f0471ea3a10d4d3b2f1dfba4d58f422cd90aab06
gholdich <gholdich@outlook.com>
2017-05-05 12:30:41 +0000

    Stage 3: Boat object created that implements vehicle interface.

---
+++ src/main/com/tyha/objects/Boat.java
@@ -0,0 +1,47 @@
+package main.com.tyha.objects;
+
+import main.com.tyha.interfaces.Vehicle;
+
+public class Boat implements Vehicle  {
+	
+	private String name;
+	private String countryOfOrigin;
+	private double size;
+	
+	
+	@Override
+	public int getSpeed() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	@Override
+	public void accelerate() {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void deccelerate() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public String getCountryOfOrigin() {
+		return countryOfOrigin;
+	}
+	public double getSize() {
+		return size;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setCountryOfOrigin(String countryOfOrigin) {
+		this.countryOfOrigin = countryOfOrigin;
+	}
+	public void setSize(double size) {
+		this.size = size;
+	}
+
+}
