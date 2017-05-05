10f415da6532f13eb9dbfcfed988039ff7e4f663
gholdich <gholdich@outlook.com>
2017-05-05 13:05:23 +0000

    Stage 4: Added Map that holds Boats and Persons to ensure a person is onboard before entering marina at that marina, and to store where a boat currenty is.

--- src/main/com/tyha/objects/Marina.java
+++ src/main/com/tyha/objects/Marina.java
@@ -1,10 +1,55 @@
 package main.com.tyha.objects;
 
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Marina {
+	
+	private int maxSpace;
+	private String name;
+	private String address;
+	Map<Boat, Person> marina = new HashMap<>();
 
-	ArrayList<Marina> marina = new ArrayList<>();
+	public Marina(int maxSpace, String name, String address) {
+		this.maxSpace = maxSpace;
+		this.name = name;
+		this.address = address;
+	}
+
+
+	public int getMaxSpace() {
+		return maxSpace;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public Map<Boat, Person> getMarina() {
+		return marina;
+	}
+	
+	public void setMaxSpace(int maxSpace) {
+		this.maxSpace = maxSpace;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public void setMarina(Map<Boat, Person> marina) {
+		this.marina = marina;
+	}
+	
+	
 	
 	
 }
